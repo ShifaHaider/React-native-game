50f99b6a317bc56d84ada1cde4c3f9976ad5e084
Shifa Haider <shifa.haider065@gmail.com>
2018-10-21 19:59:27 +0500

    react-native-game

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ export default class App extends React.Component {
   render() {
     return (
         <View style={styles.container}>
-          <Text>Open up</Text>
+          <Text>Open up App</Text>
         </View>
 );
 }
