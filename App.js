a0b789f5c9ead21bb4d610a166d2e970adc311df
Shifa Haider <shifa.haider065@gmail.com>
2018-10-21 22:55:27 +0500

    react-native-game

--- App.js
+++ App.js
@@ -1,11 +1,23 @@
 import React from 'react';
-import { StyleSheet, Text, View , ImageBackground  , Button , Image } from 'react-native';
+import { StyleSheet, Text, View , ImageBackground  , Button , Image,
+    TouchableOpacity , Alert} from 'react-native';
 
 export default class App extends React.Component {
+
+
+
+
+  handlePress(evt){
+    Alert.alert(`x coord = ${evt.nativeEvent.locationX}`);
+    Alert.alert(`y coord = ${evt.nativeEvent.locationY}`);
+  }
   render() {
     return (
         <View style={styles.container}>
-          <Image></Image>
+          <TouchableOpacity onPress={(evt) => this.handlePress(evt)}>
+            <Image source={require('./images (2).jpg')} style={{resizeMode:'cover'}}>
+            </Image>
+          </TouchableOpacity>
         </View>
 );
 }
