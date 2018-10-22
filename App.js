71d126271ed9b692480b13c468f809bdf5fca08c
Shifa Haider <shifa.haider065@gmail.com>
2018-10-22 13:03:37 +0500

    continue app ...

--- App.js
+++ App.js
@@ -5,38 +5,40 @@ import { StyleSheet, Text, View , ImageBackground  , Button , Image,
 export default class App extends React.Component {
 
 
+    handlePress(evt) {
+        Alert.alert(`x coord = ${evt.nativeEvent.locationX}`);
+        Alert.alert(`y coord = ${evt.nativeEvent.locationY}`);
+    }
 
+    const ImagesExample = () => (
+        <Image source={require('C:/Users/Tutorialspoint/Desktop/NativeReactSample/logo.png')}/>
+    );
 
-  handlePress(evt){
-    Alert.alert(`x coord = ${evt.nativeEvent.locationX}`);
-    Alert.alert(`y coord = ${evt.nativeEvent.locationY}`);
-  }
-  const ImagesExample = () => (
-      <Image source = {require('C:/Users/Tutorialspoint/Desktop/NativeReactSample/logo.png')} />
-  );
-  render() {
-    return (
-        <View style={styles.container}>
-          <TouchableOpacity onPress={(evt) => this.handlePress(evt)}>
-            {this.ImagesExample()}
-          </TouchableOpacity>
-        </View>
-);
-}
+    render() {
+        return (
+            <ImageBackground source={require('./images (2).jpg')}
+                             style={{resizeMode:'cover', height:null,  width:null }}>
+                <View style={styles.container}>
+                    <TouchableOpacity onPress={(evt) => this.handlePress(evt)}>
+                    </TouchableOpacity>
+                </View>
+            </ImageBackground>
+        );
+    }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    //backgroundColor: 'red',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  backgroundImage: {
-    flex: 1,
-    width: '100%',
-    height: '100%',
-    resizeMode: 'cover'
-  },
+    container: {
+        flex: 1,
+        //backgroundColor: 'red',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    backgroundImage: {
+        flex: 1,
+        width: '100%',
+        height: '100%',
+        resizeMode: 'cover'
+    },
 
 });
