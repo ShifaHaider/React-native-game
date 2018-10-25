4df59aebde2715ad2b2cfc6934ca5bb38bfaaba9
Shifa Haider <shifa.haider065@gmail.com>
2018-10-25 21:40:47 +0500

    catch the egg game

--- App.js
+++ App.js
@@ -24,7 +24,6 @@ export default class App extends React.Component {
                 this.setState({imageTop: top});
             }
             else {
-                //clearInterval(i);
                 top = 0
             }
         })
