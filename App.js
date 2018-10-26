8499910011ec29f9a6db4854dd09f1f26c8b1ff9
Shifa Haider <shifa.haider065@gmail.com>
2018-10-26 22:13:22 +0500

    react-native-game

--- App.js
+++ App.js
@@ -35,7 +35,7 @@ export default class App extends React.Component {
         setInterval(()=>{
             var left = Math.floor(Math.random() * 100);
             steps.push(<Egg left={left} key={steps.length} />);
-            //this.setState({eggs: steps});
+            this.setState({eggs: steps});
         } , 2000);
 
         return (
