56a8247d3d65c92953771c80ada4f7bbe39d84fb
Shifa Haider <shifa.haider065@gmail.com>
2018-10-28 20:12:47 +0500

    continue game...

--- App.js
+++ App.js
@@ -49,7 +49,7 @@ export default class App extends React.Component {
                         e
                     )
                 })}
-hello
+hell
                 <Image style={{height: 80, width: 160, position: 'absolute', bottom : 2}}
                        source={require('./assets/basket.jpg')}/>
             </ImageBackground>
