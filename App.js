5f29e71ae07d9a304e0be593ea2cd47b2957055c
Shifa Haider <shifa.haider065@gmail.com>
2018-10-29 22:10:42 +0500

    vibration mobile

--- App.js
+++ App.js
@@ -26,7 +26,7 @@ export default class App extends React.Component {
         );
     }
 }
-Hellooooooooooooooooooooooooooooooooooo
+Hellooooooooooooooooooooooooooooooooo
 const styles = StyleSheet.create({
     container: {
         flex: 1,
